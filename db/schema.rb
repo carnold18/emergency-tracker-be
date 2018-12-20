7bfcf8d3f970f0ad3c2675a1c1bc93f7d9325b08
Christopher Arnold <chrisarnold2010@gmail.com>
2018-12-20 00:12:53 -0600

    updated seeds and added HPL user

--- db/schema.rb
+++ db/schema.rb
@@ -10,57 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_211947) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "message"
-    t.integer "status"
-    t.bigint "zone_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["zone_id"], name: "index_posts_on_zone_id"
-  end
-
-  create_table "user_zones", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "zone_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_user_zones_on_user_id"
-    t.index ["zone_id"], name: "index_user_zones_on_zone_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.string "address_line_1"
-    t.string "address_line_2"
-    t.string "city"
-    t.string "state"
-    t.string "zip_code"
-    t.string "country"
-    t.float "lat"
-    t.float "lng"
-    t.integer "user_type"
-    t.integer "status"
-    t.string "password_digest"
-    t.bigint "zone_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["zone_id"], name: "index_users_on_zone_id"
-  end
-
-  create_table "zones", force: :cascade do |t|
-    t.string "country"
-    t.string "state"
-    t.string "zip_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
